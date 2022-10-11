e9a9972982c11d8ff45d8ffdbbff440c999ed78e
Daryl Rogony <darylrogony@gmail.com>
2022-10-11 14:39:33 +0300

    Fit: add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
+// import Dashboard from './components/dashboard/Dashboard';
+import SignUp from './components/SignUps/SignUp';
+import SignIn from './components/SignUps/SignIn';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-     <Dashboard />
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
