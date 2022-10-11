c0c7050edbbcc97ab8ee6dbdfb01f675620625ce
Daryl Rogony <darylrogony@gmail.com>
2022-10-11 16:45:03 +0300

    Fix: remove avatar

--- src/components/SignUps/SignIn.js
+++ src/components/SignUps/SignIn.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -10,7 +9,6 @@ import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
@@ -66,9 +64,6 @@ export default function SignIn() {
               alignItems: 'center',
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
@@ -118,7 +113,7 @@ export default function SignIn() {
                 </Grid>
               </Grid>
             </Box>
-          </Box>
+          </Box>  
         </Grid>
       </Grid>
     </ThemeProvider>
