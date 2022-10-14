3ff2bcbeb915d4c7b74237fa2d0b293f8c16ea4a
Daryl Rogony <darylrogony@gmail.com>
2022-10-14 05:51:00 +0300

    Fit: handle submit

--- src/components/SignUps/SignIn.js
+++ src/components/SignUps/SignIn.js
@@ -70,7 +70,7 @@ export default function SignIn({onLogin}) {
             backgroundPosition: 'center',
           }}
         />
-        <Grid  onSubmit={handleSubmit} item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
               my: 8,
@@ -83,7 +83,7 @@ export default function SignIn({onLogin}) {
             <Typography component="h1" variant="h5">
               Login
             </Typography>
-            <Box component="form" noValidate  sx={{ mt: 1 }}>
+            <Box  onSubmit={handleSubmit}  component="form" noValidate  sx={{ mt: 1 }}>
               <TextField
                 margin="normal"
                 required
